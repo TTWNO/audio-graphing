ce67cade789b610527a070c2c4c0bdad68be398a
Tait Hoyem <tait@tait.tech>
2025-01-11 14:08:34 -0700

    Combine multiple animation directives in one file

--- aplot/aplot-bin/src/main.rs
+++ aplot/aplot-bin/src/main.rs
@@ -9,12 +9,26 @@ fn main() {
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
+    let line1 = aplot::Linear {
+        p1: Point { x: 10.0, y: 10.0*8.0 },
+        p2: Point { x: 10.0, y: 150.0*8.0 },
+    };
+    let line2 = aplot::Linear {
+        p1: Point { x: 10.0, y: 150.0*8.0 },
+        p2: Point { x: 10.0, y: 80.0*8.0 },
+    };
     let quad = aplot::Quadradic {
         p1: Point { x: 10.0, y: 80.0*8.0 },
         p2: Point { x: 52.5, y: 10.0*8.0 },
-        p3: Point { x: 10.0, y: 80.0*8.0 },
+        p3: Point { x: 90.0, y: 80.0*8.0 },
     };
     let mut writer = hound::WavWriter::create("out.wav", spec).unwrap();
+    for sample in line1.samples(48_000.0, 0.25) {
+        writer.write_sample(sample).unwrap();
+    }
+    for sample in line2.samples(48_000.0, 0.25) {
+        writer.write_sample(sample).unwrap();
+    }
     for sample in quad.samples(48_000.0, 2.0) {
         writer.write_sample(sample).unwrap();
     }
