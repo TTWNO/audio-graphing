71df646a7ea779ae11987bc42394a46f65712c31
Tait Hoyem <tait@tait.tech>
2025-07-27 17:01:07 -0600

    Fix up bin

--- aplot/aplot-bin/src/main.rs
+++ aplot/aplot-bin/src/main.rs
@@ -1,9 +1,38 @@
 use aplot;
 use hound;
-use aplot::{Segments, Config, PlottingFunc};
+use aplot::{
+    //Segments,
+    Config, PlottingFunc};
 
 const SVG_PATH: &'static str = "M 10 80 Q 52.5 10, 55 80 T 180 80";
 const EXPR: &'static str = "x^2";
+
+use fasteval::{Evaler, Compiler, Slab, Parser, Instruction};
+
+pub struct DynamicPlottingFunc {
+    slab: Slab,
+    inner: Instruction,
+}
+
+impl DynamicPlottingFunc {
+    pub fn from_str(s: &str) -> Result<Self, fasteval::Error> {
+        let parser = Parser::new();
+        let mut slab = Slab::new();
+        let compiled = parser.parse(s, &mut slab.ps)?.from(&slab.ps).compile(&slab.ps, &mut slab.cs);
+        Ok(Self { inner: compiled, slab })
+    }
+    pub fn fn_mut(&mut self) -> impl FnMut(f64) -> Result<f64, fasteval::Error>  + use<'_> {
+        |x: f64| {
+         let mut cb = move |name:&str, _args:Vec<f64>| -> Option<f64> {
+             match name {
+                 "x" => Some(x),
+                 _ => None,
+             }
+         };
+        self.inner.eval(&self.slab, &mut cb)
+        }
+    }
+}
 
 fn main() {
     let config = Config {
@@ -37,8 +66,9 @@ fn main() {
     //    });
     
     /// From math expression to SVG path and audio samples
-    let expr = PlottingFunc::from_str(EXPR).unwrap();
-    println!("<path id=\"p2\" transform=\"translate(100, 100)\" d=\"{}\" stroke=\"navy\" fill=\"transparent\"/>", expr.to_path_segments(config.clone(), 250, 1.0, 1.0).unwrap());
+    let mut expr_ = DynamicPlottingFunc::from_str(EXPR).unwrap();
+    let mut expr = PlottingFunc::new(expr_.fn_mut());
+    //println!("<path id=\"p2\" transform=\"translate(100, 100)\" d=\"{}\" stroke=\"navy\" fill=\"transparent\"/>", expr.to_path_segments(config.clone(), 250, 1.0, 1.0).unwrap());
     expr.all_samples(config.clone(), 1.0)
         .unwrap()
         .for_each(|samp| {
